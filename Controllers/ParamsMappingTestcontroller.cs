c3e3cdc8d97ad72176c49e69bb45a02f40e304f2
kenshukubo <kensyu124logic@yahoo.co.jp>
2021-07-09 21:56:36 +0900

    model list アクション

--- Controllers/ParamsMappingTestcontroller.cs
+++ Controllers/ParamsMappingTestcontroller.cs
@@ -2,6 +2,8 @@ using System;
 using Microsoft.AspNetCore.Mvc;
 // Model使う際に必要なおまじない
 using helloweb.Models;
+// List使う際に必要なおまじない
+using System.Collections.Generic;
 
 namespace helloweb.Controllers {
     public class ParamsMappingTestcontroller : Controller {
@@ -20,6 +22,10 @@ namespace helloweb.Controllers {
 
         public IActionResult GetPerson (Person person) {
             return Json (new { message = "Action params mapping", data = person });
+        }
+
+        public IActionResult GetPersonList (List<Person> persons) {
+            return Json (new { message = "Action params mapping", data = persons });
         }
     }
 }
