ec232de998d7873a5b0fd46bdac2d45738c251f1
kenshukubo <kensyu124logic@yahoo.co.jp>
2021-07-09 22:27:24 +0900

    viewを表示 modelから取得して表示

--- Model/Person.cs
+++ Model/Person.cs
@@ -4,6 +4,8 @@ namespace helloweb.Models {
     public class Person {
         public string Name { get; set; }
         public int Age { get; set; }
+        public DateTime Birthday { get; set; }
+        public string[] Hobby { get; set; }
 
     }
 }
